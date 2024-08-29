5e5e63dc4d97a78c754d86e895da973beeb5eaac
Kevin Guzmán <kevinguzman@Kevins-MacBook-Pro.local>
2024-08-28 23:46:55 -0500

    feat: maps works

--- src/presentation/views/Register/index.tsx
+++ src/presentation/views/Register/index.tsx
@@ -1,5 +1,7 @@
 import { Text } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 import { AppView } from '@presentation/shared/components/AppView';
 
 import { Props } from './props.ts';
@@ -8,6 +10,15 @@ export const Register = ({}: Props) => {
   return (
     <AppView>
       <Text>Register</Text>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{ flex: 1 }}
+      />
     </AppView>
   );
 };
